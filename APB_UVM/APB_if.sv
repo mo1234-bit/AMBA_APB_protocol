71f08997098829bb7e7b20ef870a3e24c590f0a5
Mohamed Gamal Mohamed Diab <mj26282268@gmail.com>
2025-10-23 14:41:22 +0300

    Delete APB_UVM directory

--- APB_UVM/APB_if.sv
+++
@@ -1,9 +0,0 @@
-interface APB_if (PCLK);
-input PCLK;
-logic PRESETn,transfer,READ_WRITE;
-logic [8:0] apb_write_paddr;
-logic [7:0]apb_write_data;
-logic [8:0] apb_read_paddr;
-logic PSLVERR,PSLVERR_ref;
-logic [7:0] apb_read_data_out,apb_read_data_out_ref;
-endinterface : APB_if
--- APB_UVM/ram_if.sv
+++
@@ -1,8 +0,0 @@
-import pkg::*;
-interface ram_if (PCLK);
-input PCLK;
-logic PRSTn,PENABLE,PWRITE,PSEL;
-logic[7:0]PADDR,PWDATA;
-logic[7:0]PRDATA1,PRDATA1_ref;
-logic PREADY,PREADY_ref;
-endinterface 
