d61f6c959b6dcd9c1bf90a0ae68e8f34496972c5
Mohamed Gamal Mohamed Diab <mj26282268@gmail.com>
2026-03-07 23:51:45 +0200

    del unused

--- Agents/Debug_Agent/debug_sequencer.sv
+++
@@ -1,9 +0,0 @@
-
-  class debug_sequencer extends uvm_sequencer #(seq_item);
-         
-    `uvm_component_utils(debug_sequencer)
-        function  new(string name = "debug_sequencer" , uvm_component parent = null);
-        	super.new(name,parent);
-        endfunction : new
-           
-  endclass
--- interr_intf.sv
+++
@@ -1,8 +0,0 @@
-interface interrupt_intf (input clk,rst_n);
-
-   
-     logic [31:0] irq_i;  
-     logic        irq_ack_o;
-     logic [ 4:0] irq_id_o;
-
-endinterface : interrupt_intf
